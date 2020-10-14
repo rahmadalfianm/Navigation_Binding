5181af36033c2ccc1c08cbf146250a4777d64e41
rahmadalfianm <60783259+rahmadalfianm@users.noreply.github.com>
2020-10-14 10:07:07 +0700

    Fix Navigation Binding

--- app/src/main/java/com/navigation/aliza/MenuFragment.java
+++ app/src/main/java/com/navigation/aliza/MenuFragment.java
@@ -2,6 +2,7 @@ package com.navigation.aliza;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -10,24 +11,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.navigation.aliza.databinding.FragmentMenuBinding;
+
 
 public class MenuFragment extends Fragment {
-    private Button nama;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_menu, container, false);
-        nama = view.findViewById(R.id.btnNama);
-        nama.setOnClickListener(new View.OnClickListener() {
+        //View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        FragmentMenuBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_menu, container,false);
+        binding.btnNama.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Bundle bundle = new Bundle();
                 bundle.putString("nama", "My Name is Aliza Rizqi Fitriana");
-                //Navigation.findNavController(view).navigate(R.id);
+                Navigation.findNavController(view).navigate(R.id.action_menuFragment_to_detailFragment, bundle);
             }
         });
-        return view;
+        return binding.getRoot();
     }
 }
